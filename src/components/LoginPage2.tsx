462eb3d65ad83cdb5d89fa1ccb440cb7f0a89820
Darshan <chhantyaldarshan2@gmail.com>
2026-02-06 15:19:31 +0545

    2nd commit

--- src/components/LoginPage2.tsx
+++ src/components/LoginPage2.tsx
@@ -4,7 +4,7 @@ const LoginPage2 = () => {
       className="w-screen h-screen bg-gradient-to-tr from-sky-200 via-purple-200 to-pink-200
  flex justify-center items-center"
     >
-      <div className="w-75/100 h-75/100 border-none rounded-2xl shadow-2xl flex ">
+      <div className="w-75/100 h-75/100 border-none rounded-2xl shadow-2xl flex flex-wrap ">
         <div className="border-none h-full rounded-l-2xl  w-60/100 bg-[url(/156156-abstract-lavender-background.jpg)] bg-cover flex flex-col justify-center items-center gap-3">
           <h1 className="text-gray-800 font-medium"> Welcome back! </h1>
           <div className="text-gray-800 font-medium">
